b9320a40c7d87e0c53ac8e0d3d8105a0309eacbb
maxkoenike <76454833+maxkoenike@users.noreply.github.com>
2020-12-21 22:54:12 +0100

    Add files via upload

---
+++ Conrod.java
@@ -0,0 +1,38 @@
+package shopsystem;
+
+public class Conrod {
+	
+	
+	static int c1 = 12;
+	static int c2 = 9;
+	static int c3 = 5;
+	
+	static private int cp1 = 60;
+	static private int cp2 = 80;
+	static private int cp3 = 100;
+	
+	public static int getCp1() {
+		return cp1;
+	}
+	public static void setCp1(int cp1) {
+		Conrod.cp1 = cp1;
+	}
+	
+	public static int getCp2() {
+		return cp2;
+	}
+	public static void setCp2(int cp2) {
+		Conrod.cp2 = cp2;
+	}
+	
+	public static int getCp3() {
+		return cp3;
+	}
+	public static void setCp3(int cp3) {
+		Conrod.cp3 = cp3;
+	}
+	
+	
+	
+
+}
---
+++ Ring.java
@@ -0,0 +1,35 @@
+package shopsystem;
+
+public class Ring {
+	
+	
+	static int r1 = 15;
+	static int r2 = 8;
+	static int r3 = 12;
+	
+	static private int rp1 = 60;
+	static private int rp2 = 80;
+	static private int rp3 = 100;
+	
+	public static int getRp1() {
+		return rp1;
+	}
+	public static void setRp1(int rp1) {
+		Ring.rp1 = rp1;
+	}
+	
+	public static int getRp2() {
+		return rp2;
+	}
+	public static void setRp2(int rp2) {
+		Ring.rp2 = rp2;
+	}
+	
+	public static int getRp3() {
+		return rp3;
+	}
+	public static void setRp3(int rp3) {
+		Ring.rp3 = rp3;
+	}
+
+}
